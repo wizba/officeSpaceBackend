0cd6dde57995c787e958e45d5c08eaa84a860a1c
unknown <williamrabs@gmail.com>
2021-12-27 16:15:40 +0200

    app

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ const mongoose= require('mongoose');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const app=express();
+const password ='12345';
+const url= `mongodb+srv://wizba:${password}@cluster0.dbk2t.mongodb.net/test`;
 const myEnv = require('dotenv').config();
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
-mongoose.connect(process.env.MONGODB_URL,{useNewUrlParser: true});
+// process.env.MONGODB_UR
+mongoose.connect(url,{useNewUrlParser: true});
 const con= mongoose.connection;
 app.use(express.json());
 try{
@@ -26,5 +28,4 @@ var routes = require('./routes/routes.js');
 routes(app);
 app.listen(port, () =>{
     console.log('Server started');
-    
 })
